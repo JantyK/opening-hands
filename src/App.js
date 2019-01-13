5e05d6cfb0edaf47579a2aeef474adf6042a2206
Janty Kojak <janty@v1apps.com>
2019-01-13 14:31:01 -0500

    Changed logo to a temp one, installed react-router-dom and material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Opening Hand is a probability calculator for your favorite card games
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Let's Get Started
           </a>
         </header>
       </div>
